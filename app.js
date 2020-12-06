ba0bf495bc60161bcb3e22db0297d5f2898c7276
Joseph Chu <70360148+JoeChuCodes@users.noreply.github.com>
2020-12-06 15:31:43 -0500

    got rid of search.js in favor of app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Listen for all clicks on the document
+$('#hi-temp').val('See ME!')
 document.addEventListener(
   'click',
   function () {
@@ -23,3 +24,46 @@ document.addEventListener(
   false,
 )
 
+// Weather API (non Jquery)b5597dd37e39c1d17865dfa529234a43'
+// http://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=
+// const display = document.querySelector('#weatherResults')
+// const inputValue = document.querySelector('.inputValue')
+// const submitButton = document.querySelector('.submit-btn')
+
+// let city = document.querySelector('.name')
+// let icon = document.querySelector('.icon')
+// let maxTemp = document.querySelector('.hi-temp')
+// let minTemp = document.querySelector('.low-temp')
+// let humid = document.querySelector('.humidity')
+
+// submitButton.addEventListener('click', function (name) {
+//   fetch(
+//     'https://api.openweathermap.org/data/2.5/weather?q=' +
+//       inputValue.value +
+//       '&appid=ef057d984223160d5c7aff529dc1653e',
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // var tempValue = data['main']['temp']
+//       // var nameValue = data['name']
+//       // var descValue = data['weather'][0]['description']
+
+//       // main.innerHTML = nameValue
+//       // desc.innerHTML = 'Desc - ' + descValue
+//       // temp.innerHTML = 'Temp - ' + tempValue
+//       // input.value = ''
+//       console.log(data)
+//     })
+
+//     .catch((err) => alert('Wrong city name!'))
+// })
+let weather =
+  'http://api.openweathermap.org/data/2.5/weather?q=33850&appid=b5597dd37e39c1d17865dfa529234a43'
+
+let results = $.get(weather, function (data) {
+  // $('.result').html(data)
+  console.log(data)
+  alert('Load was performed.')
+})
+
+$('#hi-temp').val('Hello World')
